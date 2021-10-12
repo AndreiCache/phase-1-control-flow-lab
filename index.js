223eb3450b1a68c9fb06665e6f6f6e0ea5036373
Andrei <giurrgiu@gmail.com>
2021-10-12 19:45:13 -0400

    commit tree 3c3fd044242fec8a504cd6fa4175014b0c1a4aa0

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet > 2500){
+    return 'No can do.'
+  }else if (feet > 2000 ){
+      return 'I will gladly take your thirty bucks.'
+  }else if (feet < 400)   
+     return  'This one is on me!'
+}
+ 
+
+function ternaryCheckCity(NYC){
+  return NYC === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'  
+    default :
+      return 'Bye.'  
+  }
 }
